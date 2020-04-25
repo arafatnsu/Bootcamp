cd9e29880b88a7442f8ed672dcb718d10c593086
arafatnsu <arafatnsu@yahoo.com>
2020-04-25 21:24:31 +0300

    upperCase, lowerCase

--- Practice/Create-table/table.js
+++ Practice/Create-table/table.js
@@ -1,8 +1,8 @@
 const users = [
     { name: "Amit", city: "Berlin", age: 20 },
+    { name: "Manzu", city: "Helsinki", age: 32 },
     { name: "Arafat", city: "Turku", age: 35 },
-    { name: "Arun", city: "Turku", age: 31 },
-    { name: "Manzu", city: "Helsinki", age: 32 }
+    { name: "Arun", city: "Turku", age: 31 }
 ]
 
 function buildTable(data) {
@@ -10,7 +10,7 @@ function buildTable(data) {
 
     for (var i = 0; i < data.length; i++) {
         var row = `<tr>
-                        <td>${data[i].name}</td>
+                        <td class="name-td">${data[i].name}</td>
                         <td>${data[i].age}</td>
                         <td>${data[i].city}</td>
                     </tr>`
@@ -19,3 +19,27 @@ function buildTable(data) {
 }
 
 buildTable(users)
+
+function toUpCase() {
+    let elements = document.getElementsByClassName('name-td')
+    for (let i = 0; i < elements.length; i++) {
+        //const text = elements[i].innerHTML
+        elements[i].innerHTML = elements[i].innerHTML.toUpperCase()
+    }
+}
+
+function toLowCase() {
+    let elements = document.getElementsByClassName('name-td')
+    for (let i = 0; i < elements.length; i++) {
+        const text = elements[i].innerHTML
+        elements[i].innerHTML = text.toLowerCase()
+    }
+}
+
+function toNormal() {
+    let elements = document.getElementsByClassName('name-td')
+    for (let i = 0; i < elements.length; i++) {
+        const text = elements[i].innerHTML.toLowerCase()
+        elements[i].innerHTML = text.charAt(0).toUpperCase() + text.slice(1)
+    }
+}
