667bf22ac3dfac8d5b4f572513b7e1c4046745e1
arafatnsu <arafatnsu@yahoo.com>
2020-05-28 02:54:01 +0300

    practice

---
+++ Practice/Form-app/form.js
@@ -0,0 +1,29 @@
+const todos = [{
+    text: 'Order cat food',
+    completed: false
+}, {
+    text: 'Clean kitchen',
+    completed: true
+}, {
+    text: 'Buy food',
+    completed: true
+}, {
+    text: 'Do work',
+    completed: false
+}, {
+    text: 'Excerise',
+    completed: true
+}]
+
+
+renderTodos(todos, filters)
+
+
+document.querySelector('#new-todo').addEventListener('submit', function(e) {
+    e.preventDefault()
+    todos.push({
+        text: e.target.elements.text.value,
+        completed: false
+    })
+    renderTodos
+})
